9b53dc129bc1b18824c4404f950444c3897cc1bf
Rory Donley-Lovato <rdonleylovato@gmail.com>
2022-10-27 16:17:15 -0600

    Added webpage

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require ("http");
+
+http.createServer(function(request, response){
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<h1>Hello, Node.js!</h1>");
+    response.write("<h2>Rory Donley-Lovato - s73h545</h2>   \
+            <p>Running node server on port 3053</p>");
+    response.end();
+}).listen(3053);
